c84502fb5888d01d719c0cf76969dfc2a9063061
Lui <luih@wit.edu>
2023-10-09 20:38:36 -0400

    added long respawn toggle and improved use item timing

--- src/client/java/com/autouse/AutoUseModClient.java
+++ src/client/java/com/autouse/AutoUseModClient.java
@@ -13,12 +13,15 @@ public class AutoUseModClient implements ClientModInitializer {
 	private static AutoUseModClient instance;
 
 	public static KeyBinding autoUseBind;
+	public static KeyBinding longRespawnBind;
 	private static boolean autoUseIsOn = false;
+	private static boolean longRespawnOn = false;
 	@Override
 	public void onInitializeClient() {
 		if (instance == null) instance = this;
 
-		autoUseBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("autoUseKey", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_V, "AutoUse"));
+		autoUseBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("Toggle Auto Use", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_V, "AutoUse"));
+		longRespawnBind = KeyBindingHelper.registerKeyBinding(new KeyBinding("Toggle Long Respawn", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_N, "AutoUse"));
 
 		//Register Tick Callback
 		ClientTickEvents.END_CLIENT_TICK.register(this::tick);
@@ -29,9 +32,17 @@ public class AutoUseModClient implements ClientModInitializer {
 			autoUseIsOn = !autoUseIsOn;
 			MinecraftClient.getInstance().player.sendMessage(Text.of("Autouse set to: " + autoUseIsOn));
 		}
+		if (longRespawnBind.wasPressed()) {
+			longRespawnOn = !longRespawnOn;
+			MinecraftClient.getInstance().player.sendMessage(Text.of("Long respawn set to: " + longRespawnOn));
+		}
 	}
 
 	public static boolean isAutoUseOn() {
 		return autoUseIsOn;
+	}
+
+	public static boolean isLongRespawnOn() {
+		return longRespawnOn;
 	}
 }
--- src/client/java/com/autouse/mixin/client/PlayerEntityMixin.java
+++ src/client/java/com/autouse/mixin/client/PlayerEntityMixin.java
@@ -17,8 +17,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin extends LivingEntity {
 
-    private int respawnDelayTimer = 100; // timer for respawn
-    private int useItemDelayTimer = 120; // timer for useItem
+    private int respawnDelayTimer = 2000; // timer for respawn
+    private int useItemDelayTimer = 200; // timer for useItem
     private int tickDelayTimer = 100; // timer for tick
     private boolean shouldUseItem = false;
     private boolean shouldRespawn = false;
@@ -36,8 +36,14 @@ public abstract class PlayerEntityMixin extends LivingEntity {
             player = MinecraftClient.getInstance().player;
 
             if (player != null && player.isDead()) {
+                System.out.println("DEAD Calling respawn");
+                if (AutoUseModClient.isLongRespawnOn()) {
+                    System.out.println("setting delay timer to 12000");
+                    respawnDelayTimer = 120000; // set to 10 minutes
+
+                }
                 doRequestRespawn();
-            } else if (tickDelayTimer % 99 == 0 && tickDelayTimer != 0) {
+            } else if (tickDelayTimer % 99 == 0 && tickDelayTimer != 0 && player.experienceLevel < 50) {
                 player.getInventory().selectedSlot = 0; // reset to first hotbar slot every once in a while so we aren't stuck on the 2nd slot;
                 tickDelayTimer--;
             }
@@ -45,6 +51,7 @@ public abstract class PlayerEntityMixin extends LivingEntity {
                 tickDelayTimer--;
             } else {
                 if (player != null && player.experienceLevel >= 50 && notUsedYet) {
+                    player.getInventory().selectedSlot = 0;
                     notUsedYet = false;
                     useItem();
                 }
@@ -57,21 +64,43 @@ public abstract class PlayerEntityMixin extends LivingEntity {
         shouldRespawn = true;
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             // Handle the delay
-            if (shouldRespawn && respawnDelayTimer > 0 && respawnDelayTimer != 40) {
-                respawnDelayTimer--;
-            } else if (shouldRespawn && respawnDelayTimer == 40) {
-                player.sendMessage(Text.of("You died, requesting respawn"));
-                // Send the respawn request after 100 ticks
-                player.requestRespawn();
-                respawnDelayTimer--;
-            }
-            else if (shouldRespawn) {
-                // Send the "/home" message after 100 ticks
-                player.sendMessage(Text.of("calling /home xp"));
-                player.networkHandler.sendChatCommand("home xp"); // make sure you `/sethome xp` first. TODO: Make this field customizable.
-                shouldRespawn = false;
-                player.getInventory().selectedSlot = 0; // reset to first hotbar slot after death so we aren't stuck on the 2nd slot
-                respawnDelayTimer = 100;  // 100 ticks
+            if (AutoUseModClient.isLongRespawnOn()) {
+                if (shouldRespawn && respawnDelayTimer > 0 && respawnDelayTimer != 15000) {
+                    System.out.println("respawnDelayTimer" + respawnDelayTimer);
+                    respawnDelayTimer--;
+                } else if (shouldRespawn && respawnDelayTimer == 15000) {
+                    player.sendMessage(Text.of("You died, requesting respawn"));
+                    System.out.println("requesting respawn respawnDelayTimer" + respawnDelayTimer);
+                    player.requestRespawn();
+                    respawnDelayTimer--;
+                }
+                else if (shouldRespawn) {
+                    // Send the "/home" message after 100 ticks
+                    System.out.println("calling home respawnDelayTimer" + respawnDelayTimer);
+                    player.sendMessage(Text.of("calling /home xp"));
+                    player.networkHandler.sendChatCommand("home xp"); // make sure you `/sethome xp` first. TODO: Make this field customizable.
+                    shouldRespawn = false;
+                    player.getInventory().selectedSlot = 0; // reset to first hotbar slot after death so we aren't stuck on the 2nd slot
+                }
+            } else {
+                if (shouldRespawn && respawnDelayTimer > 0 && respawnDelayTimer != 500) {
+                    System.out.println("respawnDelayTimer" + respawnDelayTimer);
+                    respawnDelayTimer--;
+                } else if (shouldRespawn && respawnDelayTimer == 500) {
+                    player.sendMessage(Text.of("You died, requesting respawn"));
+                    System.out.println("requesting respawn respawnDelayTimer" + respawnDelayTimer);
+                    player.requestRespawn();
+                    respawnDelayTimer--;
+                }
+                else if (shouldRespawn) {
+                    // Send the "/home" message after 100 ticks
+                    System.out.println("calling home respawnDelayTimer" + respawnDelayTimer);
+                    player.sendMessage(Text.of("calling /home xp"));
+                    player.networkHandler.sendChatCommand("home xp"); // make sure you `/sethome xp` first. TODO: Make this field customizable.
+                    shouldRespawn = false;
+                    player.getInventory().selectedSlot = 0; // reset to first hotbar slot after death so we aren't stuck on the 2nd slot
+                    respawnDelayTimer = 1000;  // 100 ticks
+                }
             }
         });
     }
@@ -97,8 +126,8 @@ public abstract class PlayerEntityMixin extends LivingEntity {
                 player.sendMessage(Text.of("Item used"));
                 shouldUseItem = false;
                 notUsedYet = true;
-                player.getInventory().selectedSlot = 0;
-                useItemDelayTimer = 120;  // 120 ticks
+//                player.getInventory().selectedSlot = 0;
+                useItemDelayTimer = 200;  // 120 ticks
             }
         });
     }
